41608d382212714dbe80409d2f5e395aa29b228b
lucas <laraihnatowicz@Lucas-MacBook-Pro.local>
2020-04-20 13:51:27 +1000

    Add core GraphQL support

--- src/GqlExtend.php
+++ src/GqlExtend.php
@@ -1,8 +1,8 @@
 <?php
 namespace today\gqlextend;
 
-use yii\base\Event;
-use markhuot\CraftQL\Types\VolumeInterface;
+use today\gqlextend\lib\GqlExtendCraftql;
+use today\gqlextend\lib\GqlExtendGraphql;
 
 class GqlExtend extends \craft\base\Plugin
 {
@@ -10,172 +10,10 @@ class GqlExtend extends \craft\base\Plugin
     {
         parent::init();
 
-		$this->addFields();
-    }
+        GqlExtendGraphql::init();
 
-    function addFields ()
-    {
-        Event::on(
-            \markhuot\CraftQL\Types\EntryInterface::class,
-            \markhuot\CraftQL\Events\AlterSchemaFields::EVENT,
-            [$this, 'extendSchema']
-        );
-
-        Event::on(
-            \markhuot\CraftQL\Types\CategoryInterface::class,
-            \markhuot\CraftQL\Events\AlterSchemaFields::EVENT,
-            [$this, 'extendSchema']
-        );
-    }
-
-    function extendSchema (\markhuot\CraftQL\Events\AlterSchemaFields $event) {
-        $field = $event->sender;
-
-        // Add a relative link to be used in app
-        $event->schema->addStringField('href')
-            ->resolve(function ($root, $args) {
-                return $root->uri === '__home__' ? '/' : '/' . $root->uri . '/';
-            });
-
-        // Resolve link text
-        $event->schema->addStringField('linkText')
-            ->resolve(function ($root, $args) {
-                return $root->__isset('linkText') ? $root->getFieldValue('linkText') : '';
-            });
-
-        // Resolve link description
-        $event->schema->addStringField('linkDescription')
-            ->resolve(function ($root, $args) {
-                return $root->__isset('linkDescription') ? $root->getFieldValue('linkDescription') : '';
-            });
-
-        // Resolve introduction
-        $event->schema->addStringField('introduction')
-        ->resolve(function ($root, $args) {
-            return $root->__isset('introduction') ? $root->getFieldValue('introduction') : '';
-        });
-
-        $breadcrumbType = $event->schema->createObjectType('BreadcrumbItem');
-        $breadcrumbType->addStringField('title');
-        $breadcrumbType->addStringField('href');
-
-        $event->schema->addField('breadcrumbs')
-            ->lists()
-            ->type($breadcrumbType)
-            // ->description('Array of entries to show the current position within the site structure.')
-            // ->values(['title' => 'Title of the entry', 'href' => 'Relative link of the entry'])
-            ->resolve(function ($root, $args) {
-                // Build the breadcrumb trail
-                $breadcrumbs = GqlExtend::addBreadcrumb($root);
-
-                // Add home page
-                array_push($breadcrumbs, array(
-                    'title' => 'Home',
-                    'href' => '/'
-                ));
-
-                return array_reverse($breadcrumbs);
-            });
-
-        $imageType = $event->schema->createObjectType('Image');
-        $imageType->addStringField('src');
-        $imageType->addStringField('srcset');
-        $imageType->addStringField('alt');
-        $imageType->addStringField('position');
-
-        $socialType = $event->schema->createObjectType('Social');
-        $socialType->addStringField('title');
-        $socialType->addStringField('description');
-        $socialType->addField('image')->type($imageType);
-
-        $seoType = $event->schema->createObjectType('SEO');
-        $seoType->addStringField('title');
-        $seoType->addStringField('description');
-        $seoType->addStringField('keywords');
-        $seoType->addField('social')->type($socialType);
-        $seoType->addBooleanField('noindex');
-        $seoType->addBooleanField('nofollow');
-
-        // Add featured image
-        $event->schema->addField('thumbnail')
-            ->type($imageType)
-            ->resolve(function ($root, $args) {
-                $asset = $root->__isset('featuredImage') && $root->getFieldValue('featuredImage') ? $root->getFieldValue('featuredImage')->one() : false;
-
-                if (!$asset) {
-                    return null;
-                }
-
-                $alt = $asset ? $asset->title : '';
-                $src = $asset ? $asset->url : '';
-                $srcset = '';
-                $optimizedImages = $asset ? $asset->optimizedImages : false;
-
-                if ($optimizedImages) {
-                    $srcset = array();
-
-                    foreach ($optimizedImages->optimizedImageUrls as $key=>$value) {
-                        array_push($srcset, $value . ' ' . $key . 'w');
-                    };
-
-                    $srcset = implode($srcset, ', ');
-                }
-
-                return array(
-                    'src' =>  $src,
-                    'srcset' => $srcset,
-                    'alt' => $asset->title,
-                    'position' => ($asset->focalPoint['x'] * 100) . '% ' . ($asset->focalPoint['y'] * 100) . '%'
-                );
-            });
-
-        // Add SEO
-        $event->schema->addField('seo')
-            ->type($seoType)
-            ->resolve(function ($root, $args) {
-                $title = $root->__isset('seoTitle') && $root->getFieldValue('seoTitle') ? $root->getFieldValue('seoTitle') : $root->title;
-                $introduction = $root->__isset('introduction') ? $root->getFieldValue('introduction') : '';
-                $description = $root->__isset('seoDescription') && $root->getFieldValue('seoDescription') ? $root->getFieldValue('seoDescription') : $introduction;
-                $asset = $root->__isset('featuredImage') && $root->getFieldValue('featuredImage') ? $root->getFieldValue('featuredImage')->one() : false;
-                $image = null;
-
-                // Add site name
-                $title .= " | " . $root->getSite()->name;
-
-                if ($asset) {
-                    $image = array(
-                        'alt' => $asset->title,
-                        'src' => $asset->url
-                    );
-                };
-
-                return array(
-                    'title' => $title,
-                    'description' => $description,
-                    'keywords' => $root->__isset('seoKeywords') ? $root->getFieldValue('seoKeywords') : '',
-                    'social' => array(
-                        'title' => $title,
-                        'description' => $description,
-                        'image' => $image
-                    ),
-                    'noindex' => $root->__isset('noindex') ? $root->getFieldValue('noindex') : false,
-                    'nofollow' => $root->__isset('nofollow') ? $root->getFieldValue('nofollow') : false,
-                );
-            });
-    }
-
-    static function addBreadcrumb($entry, $breadcrumbs = array()) {
-        array_push($breadcrumbs, array(
-            'title' => $entry->title,
-            'href' => '/' . $entry->uri . '/'
-        ));
-
-        $parent = $entry->getParent();
-
-        if ($parent) {
-            return GqlExtend::addBreadcrumb($parent, $breadcrumbs);
+        if (\Craft::$app->plugins->isPluginInstalled('markhuot/craftql')) {
+            GqlExtendCraftql::init();
         }
-
-        return $breadcrumbs;
     }
 }
---
+++ src/lib/Graphql.php
@@ -0,0 +1,195 @@
+<?php
+namespace today\gqlextend\lib;
+
+use yii\base\Event;
+use markhuot\CraftQL\Types\VolumeInterface;
+use craft\events\DefineGqlTypeFieldsEvent;
+use craft\gql\TypeManager;
+use GraphQL\Type\Definition\Type;
+use GraphQL\Type\Definition\ResolveInfo;
+use GraphQL\Type\Definition\ObjectType;
+
+class GqlExtendGraphql
+{
+    public static function init()
+    {
+		  GqlExtendGraphql::addFields();
+    }
+
+    static function addFields ()
+    {
+        /*
+        * @event DefineGqlTypeFields The event that is triggered when GraphQL type fields are being prepared.
+        * Plugins can use this event to add, remove or modify fields on a given GraphQL type.
+        */
+        Event::on(TypeManager::class, TypeManager::EVENT_DEFINE_GQL_TYPE_FIELDS, function(DefineGqlTypeFieldsEvent $event) {
+
+            // Extend entry and category interface
+            if ($event->typeName == 'EntryInterface' || $event->typeName == 'Interface') {
+
+                // Add a relative link to be used in nuxt app
+                $event->fields['href'] = [
+                    'name' => 'href',
+                    'type' => Type::string(),
+                    'resolve' => function ($source, array $arguments, $context, ResolveInfo $resolveInfo) {
+                        return $source->uri === '__home__' ? '/' : '/' . $source->uri . '/';
+                    }
+                ];
+
+                // Add a link text to be used when linking to this entry
+                $event->fields['linkText'] = [
+                    'name' => 'linkText',
+                    'type' => Type::string(),
+                    'resolve' => function ($source, array $arguments, $context, ResolveInfo $resolveInfo) {
+                        return $source->__isset('linkText') ? $source->getFieldValue('linkText') : '';
+                    }
+                ];
+
+                // Add a link description to be used when linking to this entry
+                $event->fields['linkDescription'] = [
+                    'name' => 'linkDescription',
+                    'type' => Type::string(),
+                    'resolve' => function ($source, array $arguments, $context, ResolveInfo $resolveInfo) {
+                        return $source->__isset('linkDescription') ? $source->getFieldValue('linkDescription') : '';
+                    }
+                ];
+
+                $ImageType = new ObjectType([
+                    'name' => 'Image',
+                    'fields' => [
+                        'src' => [ 'type' => Type::string() ],
+                        'srcset' => [ 'type' => Type::string() ],
+                        'alt' => [ 'type' => Type::string() ],
+                        'position' => [ 'type' => Type::string() ]
+                    ]
+                ]);
+
+                $event->fields['thumbnail'] = [
+                    'name' => 'thumbnail',
+                    'type' => $ImageType,
+                    'resolve' => function ($source, array $arguments, $context, ResolveInfo $resolveInfo) {
+                        $asset = $source->__isset('featuredImage') && $source->getFieldValue('featuredImage') ? $source->getFieldValue('featuredImage')->first() : false;
+
+                        if (!$asset) {
+                            return null;
+                        }
+
+                        $alt = $asset ? $asset->title : '';
+                        $src = $asset ? $asset->url : '';
+                        $srcset = '';
+                        $optimizedImages = $asset ? $asset->optimizedImages : false;
+
+                        if ($optimizedImages) {
+                            $srcset = array();
+
+                            foreach ($optimizedImages->optimizedImageUrls as $key=>$value) {
+                                array_push($srcset, $value . ' ' . $key . 'w');
+                            };
+
+                            $srcset = implode($srcset, ', ');
+                        }
+
+                        return array(
+                            'src' =>  $src,
+                            'srcset' => $srcset,
+                            'alt' => $asset->title,
+                            'position' => ($asset->focalPoint['x'] * 100) . '% ' . ($asset->focalPoint['y'] * 100) . '%'
+                        );
+                    }
+                ];
+
+                $BreadcrumbType = new ObjectType([
+                    'name' => 'Breadcrumb',
+                    'fields' => [
+                        'title' => [ 'type' => Type::string() ],
+                        'href' => [ 'type' => Type::string() ],
+                    ]
+                ]);
+
+                // Add breadcrumbs
+                $event->fields['breadcrumbs'] = [
+                    'name' => 'breadcrumbs',
+                    'type' => Type::listOf($BreadcrumbType),
+                    'resolve' => function ($source, array $arguments, $context, ResolveInfo $resolveInfo) {
+                        $breadcrumbs = GqlExtendGraphql::addBreadcrumb($source);
+
+                        // Add home page
+                        array_push($breadcrumbs, array(
+                            'title' => 'Home',
+                            'href' => '/'
+                        ));
+
+                        return array_reverse($breadcrumbs);
+                    }
+                ];
+
+                $SocialType = new ObjectType([
+                    'name' => 'Social',
+                    'fields' => [
+                        'title' => [ 'type' => Type::string() ],
+                        'description' => [ 'type' => Type::string() ],
+                        'image' => [ 'type' => $ImageType ],
+                    ]
+                ]);
+
+                $SEOType = new ObjectType([
+                    'name' => 'SEO',
+                    'fields' => [
+                        'title' => [ 'type' => Type::string() ],
+                        'description' => [ 'type' => Type::string() ],
+                        'keywords' => [ 'type' => Type::string() ],
+                        'social' => [ 'type' => $SocialType ],
+                    ]
+                ]);
+
+                // Add SEO
+                $event->fields['seo'] = [
+                    'name' => 'seo',
+                    'type' => $SEOType,
+                    'resolve' => function ($source, array $arguments, $context, ResolveInfo $resolveInfo) {
+                        $title = $source->__isset('seoTitle') && $source->getFieldValue('seoTitle') ? $source->getFieldValue('seoTitle') : $source->title;
+                        $description = $source->__isset('seoDescription') && $source->getFieldValue('seoDescription') ? $source->getFieldValue('seoDescription') : ( $source->__isset('linkText') ? $source->getFieldValue('linkText') : '');
+                        $asset = $source->__isset('featuredImage') && $source->getFieldValue('featuredImage') ? $source->getFieldValue('featuredImage')->first() : false;
+                        $image = null;
+
+                        // Add site name
+                        $title .= " | " . $source->getSite()->name;
+
+                        if ($asset) {
+                            $image = array(
+                                'alt' => $asset->title,
+                                'src' => $asset->url
+                            );
+                        };
+
+                        return array(
+                            'title' => $title,
+                            'description' => $description,
+                            'keywords' => $source->__isset('seoKeywords') ? $source->getFieldValue('seoKeywords') : '',
+                            'social' => array(
+                                'title' => $title,
+                                'description' => $description,
+                                'image' => $image
+                            ),
+                        );
+                    }
+                ];
+            }
+        });
+    }
+
+    static function addBreadcrumb($entry, $breadcrumbs = array()) {
+        array_push($breadcrumbs, array(
+            'title' => $entry->title,
+            'href' => '/' . $entry->uri . '/'
+        ));
+
+        $parent = $entry->getParent();
+
+        if ($parent) {
+            return GqlExtendGraphql::addBreadcrumb($parent, $breadcrumbs);
+        }
+
+        return $breadcrumbs;
+    }
+}
